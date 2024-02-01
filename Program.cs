f55b46a2086417152b123a567994e635e919414c
diepdavid <diepdavid5@gmail.com>
2024-02-01 14:22:44 +0100

    Skapat Canvas, Snake och Position classer

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace SnakeTestGame
 {
+    public class Position
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Canvas canvas = new Canvas();
+            Snake snake = new Snake();
+            
+            bool finished = false;
+
+            while (!finished)
+            {
+                canvas.drawCanvas();
+                Console.Read();
+                snake.drawSnake();
+            }
         }
     }
 }
---
+++ Snake.cs
@@ -0,0 +1,30 @@
+﻿namespace SnakeTestGame;
+
+public class Snake
+{
+    ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+    char key = 'w';
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public Snake()
+    {
+        x = 5; 
+        y = 5;
+    }
+
+    public void drawSnake()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write("S");
+    }
+
+    public void Input()
+    {
+        if (Console.KeyAvailable)
+        {
+            key = keyInfo.KeyChar;
+        }
+    }
+}
